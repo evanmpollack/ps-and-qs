8d18cfcbdb368d0ac7d17f52dc054d83ca0db9ad
Evan Pollack <epollack@salesforce.com>
2024-03-28 19:04:56 -0400

    added boilerplate for queue test

---
+++ test/pool/queue.spec.js
@@ -0,0 +1,34 @@
+import assert from 'node:assert/strict';
+import Queue from '../../lib/pool/queue.js';
+
+describe('Queue', function() {
+    context('static methods', function() {
+        describe('#fromArray', function() {
+
+        });
+    });
+
+    context('instance methods', function() {
+        let queue;
+    
+        beforeEach(function() {
+            queue = new Queue();
+        });
+        
+        describe('#enqueue', function() {
+    
+        });
+    
+        describe('#dequeue', function() {
+    
+        });
+    
+        describe('#peek', function() {
+    
+        });
+    
+        describe('#size', function() {
+    
+        });
+    });
+});
