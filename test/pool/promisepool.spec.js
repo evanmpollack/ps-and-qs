c0befac3ae00b7ab3a487a3211bf384b223a0fd1
Evan <evanmpollack@gmail.com>
2024-03-29 15:33:02 -0400

    promisepool test boilerplate

---
+++ test/pool/promisepool.spec.js
@@ -0,0 +1,13 @@
+import PromisePool from '../../lib/pool/promisepool.js';
+
+const task = () => new Promise((resolve) => setTimeout(resolve, 500));
+
+describe('PromisePool', function() {
+    describe('init', function() {
+
+    });
+    
+    describe('#start', function() {
+
+    });
+});
