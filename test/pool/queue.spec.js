1c14f9cd379497587b324645d700886fa21d3519
Evan <evanmpollack@gmail.com>
2024-04-01 19:27:05 -0400

    refined tests for #dequeue to account for error with message, added tests for #empty

--- test/pool/queue.spec.js
+++ test/pool/queue.spec.js
@@ -50,28 +50,45 @@ describe('Queue', function() {
         });
 
         describe('#dequeue', function() {
-            it('should return the value at the front', function() {
-                const data = queue.dequeue();
-                assert.equal(data, input[0]);
+            context('queue is empty', function() {
+                it('should throw error', function() {
+                    // Find way to condense beforeEach, goal=beforeEach is used in each test
+                    const emptyQueue = new Queue();
+                    const expectedMessage = 'Operation not allowed on queue of size 0';
+                    // This bound to instance method because instance reference 
+                    // is not passed in with function reference
+                    assert.throws(emptyQueue.dequeue.bind(emptyQueue), { 
+                        message: expectedMessage 
+                    });
+                });
             });
-
-            it('size should decrease by 1', function() {
-                const previousSize = queue.size();
-                queue.dequeue();
-                assert.equal(queue.size(), previousSize - 1);
+            
+            context('queue is not empty', function() {
+                it('should return the value at the front', function() {
+                    const data = queue.dequeue();
+                    assert.equal(data, input[0]);
+                });
+    
+                it('size should decrease by 1', function() {
+                    const previousSize = queue.size();
+                    queue.dequeue();
+                    assert.equal(queue.size(), previousSize - 1);
+                });
             });
         });
 
-        describe('#peek', function() {
-            it('should return the value at the front', function() {
-                const data = queue.peek();
-                assert.equal(data, input[0]);
+        describe.skip('#empty', function() {
+            context('queue is empty', function() {
+                it('should return true', function() {
+                    const emptyQueue = new Queue();
+                    assert.ok(emptyQueue.empty());
+                });
             });
 
-            it('size should not decrease', function() {
-                const previousSize = queue.size();
-                queue.peek();
-                assert.equal(queue.size(), previousSize);
+            context('queue is not empty', function() {
+                it('should return false', function() {
+                    assert.ok(!queue.empty());
+                })
             });
         });
     });
