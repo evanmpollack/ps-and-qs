88b80f611f7f9d976ec9ef0cf35e117fe4e9ee20
Evan <evanmpollack@gmail.com>
2024-04-18 18:16:02 -0400

    MaxHeap -> Heap, increased readability of default comparator, made size and empty getters

--- playground.js
+++ playground.js
@@ -1,10 +1,16 @@
 import PriorityQueue from "./lib/weightedpool/priorityqueue.js";
-import MaxHeap from "./lib/weightedpool/maxheap.js";
+import Heap from "./lib/weightedpool/heap.js";
 
-const pq = new PriorityQueue([0, 1, 2, 3, 4, 5]);
-pq.enqueue(10);
-console.log(pq.dequeue());
+// const pq = new PriorityQueue([0, 1, 2, 3, 4, 5]);
+// pq.enqueue(10);
+// console.log(pq.dequeue());
 
-const heap = new MaxHeap([0, 1, 2, 3, 4, 5]);
-heap.insert(10);
-console.log(heap.getMax());
+const comparator = (a, b) => b - a;
+
+const heap = Heap.heapify([0, 1, 2, 3, 4, 5], comparator);
+// heap.insert(10);
+Heap.push(heap, 10, comparator);
+Heap.push(heap, 20, comparator);
+console.log(heap);
+console.log(Heap.pop(heap, comparator));
+console.log(heap);
