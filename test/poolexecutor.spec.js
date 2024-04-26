7ce071a7e22901ff59364c862bc930730bcf3200
Evan <evanmpollack@gmail.com>
2024-04-26 19:15:19 -0400

    pool executor test boilerplate

---
+++ test/poolexecutor.spec.js
@@ -0,0 +1,11 @@
+import assert from 'node:assert/strict';
+import PoolExecutor from '../lib/poolexecutor.js';
+import Queue from '../lib/pool/queue.js';
+import PriorityQueue from '../lib/weightedpool/priorityqueue.js';
+import { array } from './helpers.js';
+
+describe('PoolExecutor', function() {
+    describe('#start', function() {
+
+    });
+});
