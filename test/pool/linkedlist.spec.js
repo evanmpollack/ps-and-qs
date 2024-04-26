7b3e4b9c646375b3df620a14ad251f3670b2f92b
Evan <evanmpollack@gmail.com>
2024-04-26 17:47:12 -0400

    updated linked list tests to account for new functionality

--- test/pool/linkedlist.spec.js
+++ test/pool/linkedlist.spec.js
@@ -3,6 +3,15 @@ import LinkedList from '../../lib/pool/linkedlist.js';
 import Node from '../../lib/pool/node.js';
 
 /**
+ * Helper object that has all the possible array inputs needed for unit testing
+ */
+const array = {
+    populated: [1, 2, 3, 4],
+    empty: [],
+    invalid: 0
+};
+
+/**
  * Helper method that populates a linked list with numbers in ascending order in range [0, size). 
  * Ordered for ease of testing.
  * @param {LinkedList} linkedList - LinkedList instance that needs to be populated
@@ -11,108 +20,145 @@ import Node from '../../lib/pool/node.js';
 const populateLinkedList = (linkedList, size) => Array.from({ length: size }).map((_, i) => i).forEach(i => linkedList.insertLast(i));
 
 describe('LinkedList', function() {
-    let linkedList;
-        
-    beforeEach(function() {
-        linkedList = new LinkedList();
-    });
-
-    describe('#insertLast', function() {
-        context('when list is empty', function() {
-            it('head should be set to tail node', function() {
-                linkedList.insertLast(0);
-                // Test referential equality
-                assert.equal(linkedList.head(), linkedList.tail());
-            });
-        });
-
-        context('when list is not empty', function() {
-            beforeEach(function() {
-                // Arbitrary value that satisfies n > 0
-                const n = 1;
-                populateLinkedList(linkedList, n);
+    context('creation', function() {
+        describe('#fromArray', function() {
+            it('should maintain the order of the input array', function() {
+                const llFromArray = LinkedList.fromArray(array.populated);
+                const llAsArray = (() => {
+                    const elements = [];
+                    while(llFromArray.size) {
+                        let data = llFromArray.head.data;
+                        llFromArray.removeFirst();
+                        elements.push(data);
+                    }
+                    return elements;
+                })();
+                assert.deepEqual(llAsArray, array.populated);
             });
 
-            it('previous tail node should point to new tail node', function() {
-                const previousTail = linkedList.tail();
-                linkedList.insertLast(0);
-                assert.equal(previousTail.next, linkedList.tail());
+            it('size should increase by the length of the input array', function() {
+                assert.equal(LinkedList.fromArray(array.populated).size, array.populated.length);
             });
 
-            it('reference to head node should remain unchanged', function() {
-                const previousHead = linkedList.head();
-                linkedList.insertLast(0);
-                // Test referential equality
-                assert.equal(linkedList.head(), previousHead);
+            it('should return an empty linked list if input array is empty', function() {
+                assert.equal(LinkedList.fromArray(array.empty).size, 0);
             });
-        });
 
-        it('tail should be set to a new node', function() {
-            linkedList.insertLast(0);
-            assert(linkedList.tail() instanceof Node);
-        });
-
-        it('tail node should contain last added value', function() {
-            const insertData = 0;
-            linkedList.insertLast(insertData);
-            assert.equal(linkedList.tail().data, insertData);
-        });
-        
-        it('tail node should point to null', function() {
-            linkedList.insertLast(0);
-            assert.equal(linkedList.tail().next, null);
-        });
-
-        it('size should increase by 1', function() {
-            const previousSize = linkedList.size();
-            linkedList.insertLast(0);
-            assert.equal(linkedList.size(), previousSize + 1);
-        });
-    });
-
-    describe('#removeFirst', function() {
-        context('when list is empty', function() {
-            it('should throw error', function() {
-                const expectedErrorType = 'Error';
-                const expectedMessage = 'Operation not allowed on list of size 0';
-                // This bound to instance method because instance reference 
-                // is not passed in with function reference
-                assert.throws(linkedList.removeFirst.bind(linkedList), { 
+            it('should throw a TypeError if input is not a type of array', function() {
+                const expectedErrorType = 'TypeError';
+                const expectedMessage = 'Input must be an array';
+                assert.throws(() => LinkedList.fromArray(array.invalid), {
                     name: expectedErrorType,
-                    message: expectedMessage 
+                    message: expectedMessage
                 });
             });
         });
-
-        context('when list is not empty', function() {
-            beforeEach(function() {
-                // Arbitrary value that satisfies n > 0
-                const n = 1;
-                populateLinkedList(linkedList, n);
+    });
+    
+    context('operation', function() {
+        let linkedList;
+        
+        beforeEach(function() {
+            linkedList = new LinkedList();
+        });
+    
+        describe('#insertLast', function() {
+            context('when list is empty', function() {
+                it('head should be set to tail node', function() {
+                    linkedList.insertLast(0);
+                    // Test referential equality
+                    assert.equal(linkedList.head, linkedList.tail);
+                });
             });
-
-            it('removed node should point to null', function() {
-                const previousHead = linkedList.head();
-                linkedList.removeFirst();
-                assert.equal(previousHead.next, null);
+    
+            context('when list is not empty', function() {
+                beforeEach(function() {
+                    // Arbitrary value that satisfies n > 0
+                    const n = 1;
+                    populateLinkedList(linkedList, n);
+                });
+    
+                it('previous tail node should point to new tail node', function() {
+                    const previousTail = linkedList.tail;
+                    linkedList.insertLast(0);
+                    assert.equal(previousTail.next, linkedList.tail);
+                });
+    
+                it('reference to head node should remain unchanged', function() {
+                    const previousHead = linkedList.head;
+                    linkedList.insertLast(0);
+                    // Test referential equality
+                    assert.equal(linkedList.head, previousHead);
+                });
             });
-
-            it('head should be set to the next node', function() {
-                const next = linkedList.head().next;
-                linkedList.removeFirst();
-                assert.equal(linkedList.head(), next);
+    
+            it('tail should be set to a new node', function() {
+                linkedList.insertLast(0);
+                assert(linkedList.tail instanceof Node);
             });
-
-            it('size should decrease by 1', function() {
-                const previousSize = linkedList.size();
-                linkedList.removeFirst();
-                assert.equal(linkedList.size(), previousSize - 1);
+    
+            it('tail node should contain last added value', function() {
+                const insertData = 0;
+                linkedList.insertLast(insertData);
+                assert.equal(linkedList.tail.data, insertData);
             });
-
-            context('size is 1', function() {
-                it ('tail should be set to null', function() {
+            
+            it('tail node should point to null', function() {
+                linkedList.insertLast(0);
+                assert.equal(linkedList.tail.next, null);
+            });
+    
+            it('size should increase by 1', function() {
+                const previousSize = linkedList.size;
+                linkedList.insertLast(0);
+                assert.equal(linkedList.size, previousSize + 1);
+            });
+        });
+    
+        describe('#removeFirst', function() {
+            context('when list is empty', function() {
+                it('should throw error', function() {
+                    const expectedErrorType = 'Error';
+                    const expectedMessage = 'Operation not allowed on list of size 0';
+                    // This bound to instance method because instance reference 
+                    // is not passed in with function reference
+                    assert.throws(linkedList.removeFirst.bind(linkedList), { 
+                        name: expectedErrorType,
+                        message: expectedMessage 
+                    });
+                });
+            });
+    
+            context('when list is not empty', function() {
+                beforeEach(function() {
+                    // Arbitrary value that satisfies n > 0
+                    const n = 1;
+                    populateLinkedList(linkedList, n);
+                });
+    
+                it('removed node should point to null', function() {
+                    const previousHead = linkedList.head;
                     linkedList.removeFirst();
-                    assert.equal(linkedList.tail(), null);
+                    assert.equal(previousHead.next, null);
+                });
+    
+                it('head should be set to the next node', function() {
+                    const next = linkedList.head.next;
+                    linkedList.removeFirst();
+                    assert.equal(linkedList.head, next);
+                });
+    
+                it('size should decrease by 1', function() {
+                    const previousSize = linkedList.size;
+                    linkedList.removeFirst();
+                    assert.equal(linkedList.size, previousSize - 1);
+                });
+    
+                context('size is 1', function() {
+                    it ('tail should be set to null', function() {
+                        linkedList.removeFirst();
+                        assert.equal(linkedList.tail, null);
+                    });
                 });
             });
         });
