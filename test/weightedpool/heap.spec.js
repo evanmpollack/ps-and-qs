feb07e8910b49871d54a37b31e039b1e679d8e3b
Evan <evanmpollack@gmail.com>
2024-04-26 19:04:22 -0400

    heap test boilerplate

---
+++ test/weightedpool/heap.spec.js
@@ -0,0 +1,21 @@
+import assert from 'node:assert/strict';
+import Heap from '../../lib/weightedpool/heap.js';
+import { array } from '../helpers.js';
+
+describe('Heap', function() {
+    describe('#heapify', function() {
+
+    });
+
+    describe('#push', function() {
+
+    });
+
+    describe('#peek', function() {
+
+    });
+
+    describe('#pop', function() {
+
+    });
+});
