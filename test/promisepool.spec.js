72fccf0ceba0173d3b910d9d12a63faac27deea9
Evan <evanmpollack@gmail.com>
2024-04-26 19:28:32 -0400

    promise pool test boilerplate

---
+++ test/promisepool.spec.js
@@ -0,0 +1,40 @@
+import PromisePool from '../lib/promisepool.js';
+import { array } from './helpers.js';
+
+describe('PromisePool', function() {
+    describe('#withTasks', function() {
+
+    });
+
+    describe('#withTasks (static)', function() {
+
+    });
+
+    describe('#withConcurrency', function() {
+
+    });
+
+    describe('#withConcurrency (static)', function() {
+
+    });
+
+    describe('#withPriority', function() {
+
+    });
+
+    describe('#withPriority (static)', function() {
+
+    });
+
+    describe('#withComparator', function() {
+
+    });
+
+    describe('#withComparator (static)', function() {
+
+    });
+
+    describe('#start', function() {
+
+    });
+});
