06b87fef6a61fe33ed6bc4cbfdb8a2ddf50c53a0
Evan <evanmpollack@gmail.com>
2024-04-26 19:09:22 -0400

    priority queue test boilerplate

---
+++ test/weightedpool/priorityqueue.spec.js
@@ -0,0 +1,13 @@
+import assert from 'node:assert/strict';
+import PriorityQueue from '../../lib/weightedpool/priorityqueue.js';
+import { array } from '../helpers.js';
+
+describe('PriorityQueue', function() {
+    describe('#enqueue', function() {
+
+    });
+
+    describe('#dequeue', function() {
+
+    });
+});
