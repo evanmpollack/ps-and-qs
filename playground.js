6a9a61725502c07f7e4cb2250180cfc0b1e48ff2
Evan <evanmpollack@gmail.com>
2024-04-26 16:39:16 -0400

    deleted weighted pool class

--- playground.js
+++ playground.js
@@ -1,22 +1,6 @@
-import PriorityQueue from "./lib/weightedpool/priorityqueue.js";
-import Heap from "./lib/weightedpool/heap.js";
-import WeightedPromisePool from "./lib/weightedpool/weightedpromisepool.js";
+import PromisePool from "./lib/promisepool.js";
 
-// const pq = new PriorityQueue([0, 1, 2, 3, 4, 5]);
-// pq.enqueue(10);
-// console.log(pq.dequeue());
-
-// const comparator = (a, b) => b - a;
-
-// const heap = Heap.heapify([0, 1, 2, 3, 4, 5], comparator);
-// // heap.insert(10);
-// Heap.push(heap, 10, comparator);
-// Heap.push(heap, 20, comparator);
-// console.log(heap);
-// console.log(Heap.pop(heap, comparator));
-// console.log(heap);
-
-const task = () => new Promise((resolve) => setTimeout(() => resolve('done'), 1000));
+const task = (priority) => new Promise((resolve) => setTimeout(() => resolve(priority), 1000));
 
 const tasks = [
     {
@@ -57,14 +41,12 @@ const tasks = [
     }
 ];
 
-// const pq = new PriorityQueue(tasks, (a, b) => b.priority - a.priority)
-// console.log(pq.dequeue());
-const wp = new WeightedPromisePool(Array.from(tasks), {
-    concurrencyLimit: 2,
-    comparator: (a, b) => {
-        return b.priority - a.priority
-    }
-});
+const tasks2 = Array.from(tasks);
+
+const wp = PromisePool
+    .withPriority()
+    .withConcurrency(2)
+    .withTasks(tasks);
 
 console.time('wp');
 const results = await wp.start();
@@ -74,8 +56,8 @@ console.timeEnd('wp');
 console.time('pall');
 const limit = 2;
 const results2 = [];
-for (let i=0; i<tasks.length; i+=limit) {
-    results2.push(...(await Promise.allSettled(tasks
+for (let i=0; i<tasks2.length; i+=limit) {
+    results2.push(...(await Promise.allSettled(tasks2
         .slice(i, i + limit)
         .map(supplier => supplier.task()))));
 }
