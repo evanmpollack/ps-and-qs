f5620cceae60920e238829ab50f56a0bda3de7d4
Evan <evanmpollack@gmail.com>
2024-05-07 17:20:08 -0400

    created queueToArray helper method, prevented persistent mutations of array property values

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,33 @@
 /**
- * Helper object consisting of all possible array inputs needed for unit testing
+ * Helper object consisting of all possible array inputs needed for unit testing.
+ * - populated: [1, 2, 3, 4]
+ * - empty: []
+ * - invalid: 0
  */ 
-export const array = {
-   populated: [1, 2, 3, 4],
-   empty: [],
-   invalid: 0
+export const array = Object.defineProperties({}, {
+    populated: {
+        enumerable: true,
+        get() { return [1, 2, 3, 4]; }
+    },
+    empty: {
+        enumerable: true,
+        get() { return []; }
+    },
+    invalid: {
+        value: 0,
+        enumerable: true
+    }
+});
+
+/**
+ * Extracts each element from the given queue and 
+ * puts them into an array in the extraction order.
+ * 
+ * @param {Queue | PriorityQueue} queue 
+ * @returns {Array}
+ */
+export const queueToArray = (queue) => {
+    const result = [];
+    while(!queue.empty) result.push(queue.dequeue());
+    return result;
 };
