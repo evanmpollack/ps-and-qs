f2205f863ed563f76bd4ff0bc7429930234e2c90
Evan <evanmpollack@gmail.com>
2024-05-07 20:02:55 -0400

    test method implementation

--- test/queue/priorityqueue.spec.js
+++ test/queue/priorityqueue.spec.js
@@ -1,63 +1,131 @@
 import assert from 'node:assert/strict';
 import PriorityQueue from '../../lib/queue/priorityqueue.js';
-import { array } from '../helpers.js';
+import EmptyQueueError from '../../lib/error/emptyqueueerror.js';
+import { array, queueToArray, loadQueue } from '../helpers.js';
 
+// Max integer comparator
 const maxComparator = (a, b) => b - a;
-
-// needed?
-const isHeap = (array, comparator) => {
-    let isHeap = true;
-    const lastIntenalIndex = Math.floor((array.length / 2)) - 1;
-    for (let i = lastIntenalIndex; i>=0; i--) {
-        // Find way to eliminate duplicate definitions
-        const leftChild = {
-            get index() { return (2 * i) + 1; },
-            get value() { return array[this.index]; }
-        };
-        const rightChild = {
-            get index() { return (2 * i) + 2; },
-            get value() { return array[this.index]; }
-        }
-        const violatesHeap = (leftChild.index < array.length && comparator(array[i], leftChild.value) > 0) ||
-            (rightChild.index > array.length && comparator(array[i], rightChild.value) > 0);
-        if (violatesHeap) {
-            isHeap = false;
-            break;
-        }
-    }
-    return isHeap;
-};
 
 describe('PriorityQueue', function() {
     context('creation', function() {
         describe('#fromArray', function() {
-            it('should return an instance of PriorityQueue');
-            it('should use the ordering defined by the given comparator');
-            it('size should be equal to the size of the input array');
-            it('should return an empty priority queue if input array is empty');
-            it('should throw a TypeError if input array is not an Array');
-            it('should throw a TypeError if input comparator is not a Function');
+            it('should return an instance of PriorityQueue', function() {
+                assert(PriorityQueue.fromArray(array.populated, maxComparator) instanceof PriorityQueue);
+            });
+
+            it('should use the ordering defined by the given comparator', function() {
+                const pq = PriorityQueue.fromArray(array.populated, maxComparator);
+                assert.deepEqual(queueToArray(pq), array.populated.sort(maxComparator));
+            });
+
+            it('size should be equal to the size of the input array', function() {
+                assert.equal(PriorityQueue.fromArray(array.populated, maxComparator).size, array.populated.length);
+            });
+
+            it('should return an empty priority queue if input array is empty', function() {
+                assert.equal(PriorityQueue.fromArray(array.empty).size, array.empty.length);
+            });
+
+            it('should throw a TypeError if input array is not an Array', function() {
+                const expectedError = new TypeError('Array must be an Array');
+                assert.throws(PriorityQueue.fromArray.bind(null, array.invalid, maxComparator), expectedError);
+            });
+
+            it('should throw a TypeError if input comparator is not a Function', function() {
+                const expectedError = new TypeError('Comparator must be a Function');
+                const invalidComparator = {};
+                assert.throws(PriorityQueue.fromArray.bind(null, array.populated, invalidComparator), expectedError);
+            });
         });
     });
 
     context('operation', function() {
+        // Number not present in sample array to ensure test validity
+        const uniqueValue = Number.MAX_SAFE_INTEGER;
+        let priorityQueue;
+
+        beforeEach(function() {
+            priorityQueue = new PriorityQueue(maxComparator);
+        })
+
         describe('#enqueue', function() {
-            it('should insert the element into the priority queue');
-            it('should maintain the ordering defined by the instance\'s comparator');
-            it('size should increase by 1');
-            it('should throw a TypeError if input data is nullish');
+            // Queue enqueue test uses empty queue
+            beforeEach(function() {
+                loadQueue(priorityQueue, array.populated);
+            });
+
+            it('should insert the element into the priority queue', function() {
+                priorityQueue.enqueue(uniqueValue);
+                assert(queueToArray(priorityQueue).includes(uniqueValue));
+            });
+
+            // Does this actually test the enqueue method or does it really test dequeue?
+            // Consider a max heap, [100, 25, 50], with a broken siftUp and a working siftDown method when you insert 75.
+            // If siftUp doesn't work and then I call dequeue to get the array, the heap will fix itself on the first dequeue
+            it('should maintain the ordering defined by the instance\'s comparator', function() {
+                priorityQueue.enqueue(uniqueValue);
+                const actual = queueToArray(priorityQueue);
+                const expected = (() => {
+                    const mutableArray = array.populated;
+                    mutableArray.push(uniqueValue);
+                    return mutableArray.sort(maxComparator);
+                })();
+                assert.deepEqual(actual, expected);
+            });
+
+            it('size should increase by 1', function() {
+                const previousSize = priorityQueue.size;
+                priorityQueue.enqueue(uniqueValue);
+                assert.equal(priorityQueue.size, previousSize + 1);
+            });
+
+            it('should throw a TypeError if input data is null', function() {
+                const expectedError = new TypeError('Cannot enqueue nullish data into priority queue');
+                assert.throws(priorityQueue.enqueue.bind(priorityQueue, null), expectedError);
+            });
+
+            it('should throw a TypeError if input data is undefined', function() {
+                const expectedError = new TypeError('Cannot enqueue nullish data into priority queue');
+                assert.throws(priorityQueue.enqueue.bind(priorityQueue, undefined), expectedError);
+            });
         });
     
         describe('#dequeue', function() {
             context('priority queue is empty', function() {
-                it('should throw an EmptyQueueError');
+                it('should throw an EmptyQueueError', function() {
+                    const expectedError = new EmptyQueueError();
+                    assert.throws(priorityQueue.dequeue.bind(priorityQueue), expectedError);
+                });
             });
 
             context('priority queue is not empty', function() {
-                it('should remove the element with the most priority');
-                it('should return the element with the most priority');
-                it('should maintain the ordering defined by the instance\'s comparator');
-                it('size should decrease by 1');
+                beforeEach(function() {
+                    loadQueue(priorityQueue, array.populated);
+                });
+
+                it('should remove the element with the most priority', function() {
+                    const highestPriorityElement = array.populated.sort(maxComparator)[0];
+                    priorityQueue.dequeue();
+                    assert(!queueToArray(priorityQueue).includes(highestPriorityElement));
+                });
+
+                it('should return the element with the most priority', function() {
+                    const highestPriorityElement = array.populated.sort(maxComparator)[0];
+                    const actual = priorityQueue.dequeue();
+                    assert.equal(actual, highestPriorityElement);
+                });
+
+                // Isn't this a duplicate of should return and remove element with most priority?
+                it('should maintain the ordering defined by the instance\'s comparator', function() {
+                    const highestPriorityElement = priorityQueue.dequeue();
+                    assert.deepEqual(queueToArray(priorityQueue), array.populated.filter(v => v !== highestPriorityElement).sort(maxComparator));
+                });
+
+                it('size should decrease by 1', function() {
+                    const previousSize = priorityQueue.size;
+                    priorityQueue.dequeue();
+                    assert.equal(priorityQueue.size, previousSize - 1);
+                });
             });
         });
     });
