c7829db93dfa53243a1bb785f46aef3d2bfc014e
Evan <evanmpollack@gmail.com>
2024-05-11 01:05:40 -0400

    added function to type constant, fixed value supplier function to work with new type, implemented a function to get all non method property names from an object or class

--- test/helpers.js
+++ test/helpers.js
@@ -9,15 +9,22 @@
  * @returns {Object}
  */
 const defineImmutableValueSupplier = (o) => {
-    const config = Object.keys(o)
-        .map(k => { return { key: k, value: o[k] }; })
-        .reduce((acc, cv) => {
-            const { key, value } = cv;
-            const descriptor = (value instanceof Object) ? { get: () => structuredClone(value) } : { value };
+    const props = Object.entries(o)
+        .reduce((acc, entry) => {
+            const [ key, value ] = entry;
+            // Beware: fits testing needs, but check doesn't encapsulate all incompatible types listed in docs
+            // https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Structured_clone_algorithm#things_that_dont_work_with_structured_clone
+            const descriptor = (() => {
+                if (typeof value === 'function' || !(value instanceof Object)) {
+                    return { value };
+                } else {
+                    return { get: () => structuredClone(value) };
+                }
+            })();
             acc[key] = Object.assign(descriptor, { enumerable: true });
             return acc;
         }, {});
-    return Object.freeze(Object.defineProperties({}, config));
+    return Object.freeze(Object.defineProperties({}, props));
 }
 
 
@@ -48,6 +55,7 @@ export const type = defineImmutableValueSupplier({
     undefined: undefined,
     object: {},
     array: [],
+    function: () => {}
 });
 
 
@@ -72,3 +80,34 @@ export const queueToArray = (queue) => {
  * @param {Array} array
  */
 export const loadQueue = (queue, array) => array.forEach(v => queue.enqueue(v));
+
+// export const range = (start, end, step) => {
+//     if (start > end)
+//     const length = (step > 0) Math.abs((stop - start) / (step + 1))
+    
+//     return 
+// }
+
+export const getOwnNonMethodPropertyNames = (o, ...args) => {
+    if (o === null || o === undefined) return;
+    let descriptors;
+    let properties;
+    const target = o.prototype ?? o;
+    try {
+        const instance = new o(...args);
+        descriptors = {
+            ...Object.getOwnPropertyDescriptors(target),
+            ...Object.getOwnPropertyDescriptors(instance)
+        };
+    } catch(e) {
+        descriptors = Object.getOwnPropertyDescriptors(target);
+    } finally {
+        properties = Object.entries(descriptors)
+            .filter(([_, descriptor]) => {
+                const { get, set, value, enumerable } = descriptor;
+                return (!!set || !!get) || !(value instanceof Function && !enumerable);
+            })
+            .map(([property, _]) => property);
+    }
+    return properties;
+}
